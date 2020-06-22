22870a9dc50c4ad114a61d0752b75a585ddd257d
Jesus Duran <jesusduran@MacBook-Air-de-Jesus.local>
2020-06-22 17:27:46 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import Clap from './components/Clap'
+import Clap from './components/Clap/useAnimationHook'
+
+/**
+* ##########################################
+* >>>  Nombre de la función 
+* ##########################################
+* *  Funcionalidad 
+* todo:  Pendientes 
+*/
 
 function App() {
 	return (
 		<div className="App">
 
+			<Clap/>
 			<Clap/>
 			
 		</div>
